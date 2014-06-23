a844fc9aa914e6524be09477d42f18309b1f10a9
jarcelo <josepharcelo@gmail.com>
2014-06-23 09:58:12 -0500

    initial commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+/* Author: Joseph Arcelo
+
+*/
+
+/*===Function for Events Array====== 
+ ==================================*/
+var event = new Array;
+event[0] = "birthday";
+event[1] = "baptism";
+event[2] = "wedding";
+event[3] = "graduation";
+event[4] = "anniversary";
+event[5] = "...and any celebration";
+
+
+
